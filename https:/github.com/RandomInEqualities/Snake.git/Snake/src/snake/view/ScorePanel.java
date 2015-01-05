fe0cda9a27247652fa87aada35ad362b9ec8214e
Christian Andersen <csandersen3@gmail.com>
2015-01-05 13:19:25 +0100

    HELLO SNAKE

---
+++ https:/github.com/RandomInEqualities/Snake.git/Snake/src/snake/view/ScorePanel.java
@@ -0,0 +1,38 @@
+package snake.view;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.util.Observable;
+import java.util.Observer;
+
+import javax.swing.JPanel;
+
+import snake.model.Game;
+
+public class ScorePanel extends JPanel implements Observer {
+
+	private Game game;
+	private static final Font FONT = new Font("Serif", Font.PLAIN, 15);
+	private static final Dimension SIZE = new Dimension(400,30);
+	private static final long serialVersionUID = -8478516974010275721L;
+	
+	public ScorePanel(Game game) {
+		super();
+		this.game = game;
+		setPreferredSize(SIZE);
+	}
+	
+	public void update(Observable o, Object arg) {
+		repaint();
+	}
+	
+	protected void paintComponent(Graphics context) {
+		super.paintComponent(context);
+		context.setFont(FONT);
+		context.drawString("Score: 1 million.", 10, 20);
+	}
+	
+}
