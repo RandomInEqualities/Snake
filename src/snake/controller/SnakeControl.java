d8aab2f4c5acb258a088c1ace7e19e812c8b3085
anhvann <Anh Van@10.16.105.252>
2015-01-06 12:43:05 +0100

    Level removed from control

--- src/snake/controller/SnakeControl.java
+++ src/snake/controller/SnakeControl.java
@@ -17,42 +17,41 @@ public class SnakeControl implements KeyListener {
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-		Field head = game.getPlayer().getSnake().get(0);
-		int tsize = 100;
+		Field head = game.getPlayer().getSnake().get(0);;
 		switch (e.getKeyCode()) {
 		case 37:
 			if (dir != Direction.RIGHT) {
-				if (head.getRow() - tsize >= 0) {
-					game.getPlayer().move(-tsize, 0);
+				if (head.getRow() - 1 >= 0) {
+					game.getPlayer().move(-1, 0);
 				} else {
-					game.getPlayer().move(game.getLevel().getWidth() - tsize, 0);
+					game.getPlayer().move(game.getWidth() - 1, 0);
 				}
 				dir = Direction.LEFT;
 			}
 		case 38:
 			if (dir != Direction.DOWN) {
-				if (head.getColumn() - tsize >= 0) {
-					game.getPlayer().move(0, -tsize);
+				if (head.getColumn() - 1 >= 0) {
+					game.getPlayer().move(0, -1);
 				} else {
-					game.getPlayer().move(0, game.getLevel().getHeight() - tsize);
+					game.getPlayer().move(0, game.getHeight() - 1);
 				}
 				dir = Direction.UP;
 			}
 		case 39:
 			if (dir != Direction.LEFT) {
-				if (head.getRow() + tsize <= game.getLevel().getWidth()) {
-					game.getPlayer().move(tsize, 0);
+				if (head.getRow() + 1 <= game.getWidth()) {
+					game.getPlayer().move(1, 0);
 				} else {
-					game.getPlayer().move(-game.getLevel().getWidth() + tsize, 0);
+					game.getPlayer().move(-game.getWidth() + 1, 0);
 				}
 				dir = Direction.RIGHT;
 			}
 		case 40:
 			if (dir != Direction.UP) {
-				if (head.getColumn() + tsize <= game.getLevel().getHeight()) {
-					game.getPlayer().move(0, tsize);
+				if (head.getColumn() + 1 <= game.getHeight()) {
+					game.getPlayer().move(0, 1);
 				} else {
-					game.getPlayer().move(0, -game.getLevel().getHeight() + tsize);
+					game.getPlayer().move(0, -game.getHeight() + 1);
 				}
 				dir = Direction.DOWN;
 			}
