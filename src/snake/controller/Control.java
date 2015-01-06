95c6fc7b7ac173306ec54091bfc720b6b747bedc
anhvann <Anh Van@10.16.105.252>
2015-01-06 13:45:39 +0100

    Control update

--- src/snake/controller/Control.java
+++ src/snake/controller/Control.java
@@ -1,6 +1,5 @@
 package snake.controller;
 import java.awt.event.*;
-
 import snake.model.*;
 import snake.view.*;
 
@@ -16,54 +15,62 @@ public class Control implements KeyListener {
 		view.addKeyListener(this);
 	}
 
+	@Override
 	public void keyPressed(KeyEvent e) {
 		Field head = game.getPlayer().getSnake().get(0);;
 		switch (e.getKeyCode()) {
 		case 37:
 			if (dir != Direction.RIGHT) {
-				if (head.getRow() - 1 >= 0) {
+				if (head.getColumn() - 1 >= 0) {
 					game.getPlayer().move(-1, 0);
 				} else {
+					System.out.println("out");
 					game.getPlayer().move(game.getWidth() - 1, 0);
 				}
 				dir = Direction.LEFT;
+				break;
 			}
 		case 38:
 			if (dir != Direction.DOWN) {
-				if (head.getColumn() - 1 >= 0) {
+				if (head.getRow() - 1 >= 0) {
 					game.getPlayer().move(0, -1);
 				} else {
 					game.getPlayer().move(0, game.getHeight() - 1);
 				}
 				dir = Direction.UP;
+				break;
 			}
 		case 39:
 			if (dir != Direction.LEFT) {
-				if (head.getRow() + 1 <= game.getWidth()) {
+				if (head.getColumn() + 1 <= game.getWidth()) {
 					game.getPlayer().move(1, 0);
 				} else {
 					game.getPlayer().move(-game.getWidth() + 1, 0);
 				}
 				dir = Direction.RIGHT;
+				break;
 			}
 		case 40:
 			if (dir != Direction.UP) {
-				if (head.getColumn() + 1 <= game.getHeight()) {
+				if (head.getRow() + 1 <= game.getHeight()) {
 					game.getPlayer().move(0, 1);
 				} else {
-					game.getPlayer().move(0, -game.getHeight() + 1);
+					game.getPlayer().move(0, -game.getHeight());
 				}
 				dir = Direction.DOWN;
+				break;
 			}
 		}
 		//game.view.repaint();
 	}
 
+	@Override
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
 
 	}
 
+	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
 
