7faf6d5d6f129845f331933e12beedc56dfd1041
anhvann <Anh Van@10.16.105.252>
2015-01-07 14:19:26 +0100

    Center score board

--- src/snake/view/ScorePanel.java
+++ src/snake/view/ScorePanel.java
@@ -20,7 +20,7 @@ import snake.model.*;
 public class ScorePanel extends JPanel implements Observer {
 
 	private Score score;
-	
+	private Game game;
 	private static final Font DEFAULT_FONT = new Font("Sans_Serif", Font.BOLD, 15);
 	private static final Color PANEL_COLOUR = new Color(0.2f, 0.286f, 0.3686f);
 	private static final Color SCORE_COLOUR = new Color(0.9255f, 0.941f, 0.9451f);
@@ -29,6 +29,7 @@ public class ScorePanel extends JPanel implements Observer {
 	
 	public ScorePanel(Game game) {
 		super();
+		this.game = game;
 		setBackground(PANEL_COLOUR);
 		if (game == null) {
 			throw new NullPointerException();
@@ -55,7 +56,8 @@ public class ScorePanel extends JPanel implements Observer {
 		context.setFont(DEFAULT_FONT);
 		context.setColor(SCORE_COLOUR);
 		context.drawString("Score: " + score.getValue(), 20, 40);
-		context.drawImage(image.getScaledInstance(200, 70, Image.SCALE_SMOOTH), 300, 0, null);
+		System.out.println(getSize().width);
+		context.drawImage(image.getScaledInstance(200, 70, Image.SCALE_SMOOTH), getSize().width/2-200/2, 0, null);
 	}
 	
 }
