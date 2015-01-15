e3a53d8b251df2b9e2418a7b7e9a1d46ed041375
anna0207 <annaoelgaardnielsen@hotmail.com>
2015-01-15 14:53:34 +0100

    update

--- src/snake/control/Control.java
+++ src/snake/control/Control.java
@@ -41,7 +41,6 @@ public class Control extends KeyAdapter implements ActionListener{
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		System.out.println("pressed");
 		if (e.getActionCommand() == "mute"){
 			toggleSound();
 		}
