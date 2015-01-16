8e5fb18d114b238a259f269c6d8a3257cbc93e9d
Christian <csandersen3@gmail.com>
2015-01-16 07:47:55 +0100

    Nice! There is not need to check for the singleplayer class.

--- src/snake/view/HeaderMultiplayerPanel.java
+++ src/snake/view/HeaderMultiplayerPanel.java
@@ -6,7 +6,6 @@ import java.util.Observer;
 
 import snake.model.Event;
 import snake.model.GameMultiplayer;
-import snake.model.GameSingleplayer;
 import snake.model.Player;
 
 
@@ -57,7 +56,7 @@ public class HeaderMultiplayerPanel extends HeaderBasePanel implements Observer 
 	public void update(Observable o, Object arg) {
 		// For performance we only update the header when the game does something
 		// other than moving the snake.
-		if (o instanceof GameSingleplayer || o instanceof GameMultiplayer) {
+		if (o instanceof GameMultiplayer) {
 			Event event = (Event)arg;
 			if (event.getType() != Event.Type.MOVE) {
 				repaint();
